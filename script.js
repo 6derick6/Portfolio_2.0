fd422d49befd52458944a29642eed53802886932
Dérick <6derick6@gmail.com>
2023-02-16 22:12:59 -0300

    Criação da navegação pelo cabeçalho

---
+++ script.js
@@ -0,0 +1,19 @@
+function scrolldiv() {
+    var elem = document.getElementById("about-me");
+    elem.scrollIntoView();
+}
+
+function scrolldivSkills() {
+    var elem = document.getElementById("skills");
+    elem.scrollIntoView();
+}
+
+function scrolldivWorks() {
+    var elem = document.getElementById("works");
+    elem.scrollIntoView();
+}
+
+function scrolldivContacts() {
+    var elem = document.getElementById("contacts");
+    elem.scrollIntoView();
+}
